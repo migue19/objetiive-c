dedd0225bf31f42fb8e792e5ba46a40581208623
MiguelMex <macbookpro@macbookprodemac.lan>
2014-08-02 17:10:13 -0500

    primera version

---
+++ AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  HolaMundo
+//
+//  Created by MacBookPro on 31/07/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HolaMundo
+//
+//  Created by MacBookPro on 31/07/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
